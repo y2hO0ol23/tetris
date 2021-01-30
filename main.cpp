4af257d42ddd6316d56c22418cc7fef76f6b560d
yyhyuk <74889162+yyhyuk@users.noreply.github.com>
2021-01-30 12:40:50 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@
 
 using namespace std;
 
-#define LEFT 120
-#define RIGHT 122
-#define HARDDROP 32
-#define HOLD 99
+#define LEFT 'a'
+#define RIGHT 'd'
+#define HARDDROP 's'
+#define HOLD 'w'
 
 
 #define color(a) SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), a)
@@ -28,9 +28,13 @@ void gameset();
 void choose_block();
 void draw_map();
 void push_down();
+void erase(int line);
+
+void keyget(int key);
 
 int map[11][23];
 int block[11][23];
+int block_count[23];
 int x,y,s; //x = 좌우 y는 높이
 int next_block;
 int block_now;
@@ -224,6 +228,7 @@ int shape[7][4][4][4] = {
 int gettime;
 int checktime;
 int delay;
+int fix_delay;
 
 int main() {
 	srand(time(NULL));
@@ -234,7 +239,8 @@ int main() {
 	int key = 0;
 	next_block = rand() % 6;
 	gettime = clock();
-	delay = 2000;
+	delay = 1000;
+	fix_delay = 0;
 	gameset();
 	while (1) {
 		checktime = clock();
@@ -245,12 +251,64 @@ int main() {
 		case RIGHT:
 		case HARDDROP:
 		case HOLD:
-
-			break;
-		default: //움직이는 것이 아닐경우 키 초기화
+			keyget(key);
 			key = 0;
 		}
 		draw_map();
+	}
+}
+
+void keyget(int key) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (shape[block_now][s][i][j] == 1) {
+				map[x + i][y + j] = 0;
+			}
+		}
+	}
+	if (key == LEFT) {
+		x--;
+		int flag = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (shape[block_now][s][i][j] == 1) {
+					if (block[x + i][y + j] || x + i < 0) flag = 1;
+				}
+			}
+		}
+		if (flag == 1) x++;
+	}
+	if (key == RIGHT) {
+		x++;
+		int flag = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (shape[block_now][s][i][j] == 1) {
+					if (block[x + i][y + j] || x + i >= 10) flag = 1;
+				}
+			}
+		}
+		if (flag == 1) x--;
+	}
+	if (key == HARDDROP) {
+		int flag = 0;
+		y++;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (shape[block_now][s][i][j] == 1) {
+					if (block[x + i][y + j] || y + j >= 20) flag = 1;
+				}
+			}
+		}
+		if (flag == 1) y--;
+		if (flag == 0) gettime = clock();
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (shape[block_now][s][i][j] == 1) {
+				map[x + i][y + j] = block_now + 2;
+			}
+		}
 	}
 }
 
@@ -287,7 +345,7 @@ void choose_block() {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			if (shape[next_block][0][i][j] == 1) {
-				map[x + i][y + j] = next_block + 1;
+				map[x + i][y + j] = next_block + 2;
 			}
 		}
 	}
@@ -297,8 +355,12 @@ void choose_block() {
 		for (int j = 0; j < 4; j++) {
 			if (shape[next_block][0][i][j] == 1) {
 				gotoxy(i + 1, j + 1);
-				color(next_block + 1); printf("■");
+				color(next_block + 2); printf("■");
 				color(7);
+			}
+			else {
+				gotoxy(i + 1, j + 1);
+				printf("  ");
 			}
 		}
 	}
@@ -312,8 +374,10 @@ void draw_map() {
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 20; j++) {
 			gotoxy(i + 6, j + 1);
-			if (map[i][j] > 1) {
-				color(map[i][j]); printf("■");
+			int val = map[i][j];
+			if (!val) val = block[i][j];
+			if (val > 0) {
+				color(val); printf("■");
 				color(7);
 			}
 			else printf("  ");
@@ -330,12 +394,66 @@ void push_down() {
 		}
 	}
 	y++;
+	int flag = 0;
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			if (shape[block_now][s][i][j] == 1) {
-				map[x + i][y + j] = block_now;
+				if (block[x + i][y + j] || y + j >= 20) flag = 1;
+				map[x + i][y + j] = block_now + 2;
 			}
 		}
 	}
-	gettime = clock();
+	if (flag == 0) {
+		fix_delay = clock();
+		gettime = clock();
+	}
+	if (flag == 1) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (shape[block_now][s][i][j] == 1) {
+					map[x + i][y + j] = 0;
+				}
+			}
+		}
+		y--;
+		int flag = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (shape[block_now][s][i][j] == 1) {
+					if (block[x + i][y + j]) flag = 1;
+					map[x + i][y + j] = block_now + 2;        
+				}
+			}
+		}
+		if (checktime - fix_delay > delay) {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					if (shape[block_now][s][j][i] == 1) {
+						block[x + j][y + i] = block_now + 2;
+						block_count[y + i]++;
+					}
+				}
+				if (block_count[y + i] == 10) {
+					erase(y + i);
+					draw_map();
+				}
+				gotoxy(30, y + i);
+				printf("%2d", block_count[y + i]);
+			}
+			gettime = clock();
+			choose_block();
+		}
+	}
+}
+
+void erase(int line) {
+	for (int i = line; i > 0; i--) {
+		for (int j = 0; j < 10; j++) {
+			block[j][i] = block[j][i - 1];
+		}
+		block_count[i] = block_count[i - 1];
+	}
+	for (int j = 0; j < 10; j++) {
+		block[j][0] = 0;
+	}
 }
